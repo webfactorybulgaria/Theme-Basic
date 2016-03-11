a75cee083d97ac969d74165db8ba68d0ed85e253
Nasko <nasko@webfactory.bg>
2016-03-11 11:33:26 +0200

    inherited folder

--- src/Providers/ModuleProvider.php
+++ src/Providers/ModuleProvider.php
@@ -9,5 +9,6 @@ use TypiCMS\Modules\Theme\Providers\ModuleProvider as ServiceProvider;
 class ModuleProvider extends ServiceProvider
 {
     protected $themeName = 'basic';
-    
+    protected $sourceDir = __DIR__;
+
 }
