80d663e21ba0b981e95c1938f5ed570faa6d5514
Nasko <nasko@webfactory.bg>
2016-03-11 11:16:40 +0200

    initial commit

---
+++ src/Providers/ModuleProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TypiCMS\Modules\ThemeBasic\Providers;
+
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Foundation\Application;
+use TypiCMS\Modules\Theme\Providers\ModuleProvider as ServiceProvider;
+
+class ModuleProvider extends ServiceProvider
+{
+    protected $themeName = 'basic';
+    
+}
